a29a942958d252dd30f8cda60b056cadad7ca8d8
mauricefh <filiatreaultmaurice@gmail.com>
2022-06-13 02:33:59 -0400

    update

---
+++ Pages/Etudiant.cs
@@ -0,0 +1,11 @@
+namespace Weeks_06.Pages
+{
+    public class Etudiant
+    {
+        public int Id { get; set; }
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public int Moyenne { get; set; }
+        public string Programme { get; set; }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Weeks_06.Pages
@@ -6,10 +7,39 @@ namespace Weeks_06.Pages
     public class IndexModel : PageModel
     {
         //Data Source =.; Initial Catalog = cegep; Integrated Security = True
+        public List<Etudiant> etudiants { get; set; }
 
         public void OnGet()
         {
-
+            //Crate the list
+            etudiants = new List<Etudiant>();
+            //Create sql connection
+            using (var cn = new SqlConnection("Data Source=.;Initial Catalog=cegep; Integrated Security=true;"))
+            {
+                //Open the connection
+                cn.Open();
+                //Create query
+                string query = "SELECT * FROM Client";
+                //Create the command
+                using (var cmd = new SqlCommand(query, cn))
+                {
+                    //Create the reader
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            etudiants.Add(new Etudiant()
+                            {
+                                Id = (int)reader.GetValue(0),
+                                Prenom = (string)reader.GetValue(1),
+                                Nom = (string)reader.GetValue(2),
+                                Moyenne = (int)reader.GetValue(3),
+                                Programme = (string)reader.GetValue(4)
+                            });
+                        };
+                    }
+                }
+            }
         }
     }
 }
